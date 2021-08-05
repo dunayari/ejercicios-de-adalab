9aa3a0bc46531b8c3d4a2e4a57d040f13d14e245
ana redondo <anaredondogil@gmail.com>
2021-08-05 11:14:59 +0200

    complete the exercise

---
+++ modulo-2/modulo-2-1-ejercicio-2/main.js
@@ -0,0 +1,7 @@
+"use strict";
+document.querySelector("h1");
+let adress = document.querySelector("h1");
+adress.innerHTML = adress.innerHTML + "calle leganitos";
+document.querySelector("p");
+let hello = document.querySelector("p");
+hello.innerHTML = "Hola mundo";
