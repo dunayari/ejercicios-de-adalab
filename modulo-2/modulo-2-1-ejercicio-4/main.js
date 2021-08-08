771cb73cac1ad3d56802a8a3719993be97a278b8
ana redondo <anaredondogil@gmail.com>
2021-08-08 14:06:19 +0200

    do exercise 4

---
+++ modulo-2/modulo-2-1-ejercicio-4/main.js
@@ -0,0 +1,6 @@
+"use script";
+document.querySelector(".h1");
+const change = document.querySelector(".h1");
+const other = document.querySelector(".myName");
+const partner = document.querySelector(".partnerName");
+change.innerHTML = change.innerHTML + partner.innerHTML;
