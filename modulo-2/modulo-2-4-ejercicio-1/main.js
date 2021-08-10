29f4b1f91200565ec14fd587e8336df9c21686eb
ana redondo <anaredondogil@gmail.com>
2021-08-10 11:05:34 +0200

    do the exercise

---
+++ modulo-2/modulo-2-4-ejercicio-1/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function plus(a, b) {
+  return a * b;
+}
+
+const plusResult = plus(2, 4);
+
+console.log(plusResult);
---
+++ modulo-2/modulo-2-4-ejercicio-2/main.js
@@ -0,0 +1,8 @@
+"use strict";
+function average(a, b, c, d) {
+  return (a + b + c + d) / 4;
+}
+//debugger;
+
+const result = average(12, 5, 10, 12);
+console.log(result);
