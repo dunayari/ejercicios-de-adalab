3cf471e49f27771fcd09cfbda0fd2e1121eee9e1
ana redondo <anaredondogil@gmail.com>
2021-08-16 09:46:05 +0200

    add exercise 6

--- modulo-2/modulo-2-1-ejercicio-6/java/main.js
+++ modulo-2/modulo-2-1-ejercicio-6/java/main.js
@@ -2,5 +2,5 @@
 const main = document.querySelector(".main");
 
 const title =
-  "<h1>Lorem ipsum</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>";
+  '<h1>Lorem Ipsum</h1><img src="http://via.placeholder.com/350x150"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>';
 main.innerHTML = title;
