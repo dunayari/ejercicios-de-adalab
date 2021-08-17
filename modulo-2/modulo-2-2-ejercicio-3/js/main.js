f8f33f954354366c75226216591c3a849d77d4c7
ana redondo <anaredondogil@gmail.com>
2021-08-17 08:19:46 +0200

    add exercise 4

---
+++ modulo-2/modulo-2-2-ejercicio-3/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+const kiwis = 2;
+const kiwisPrice = 5;
+const peras = 3;
+const perasPrice = 2;
+const uvas = 1 / 2;
+const priceUvas = 4;
+console.log(kiwis * kiwisPrice + peras * perasPrice + uvas * priceUvas);
---
+++ modulo-2/modulo-2-2-ejercicio-4/main.js
@@ -0,0 +1,6 @@
+"use strict";
+const price = 128;
+const pricePerson = price / 9;
+const priceAna = pricePerson + 2;
+console.log(pricePerson);
+console.log(priceAna);
